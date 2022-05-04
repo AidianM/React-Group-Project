d5f77e8afc2be7871c4b39b7fa08f98e8a9219d3
DustinPrice <dp70117@gmail.com>
2022-05-04 10:47:13 -0500

    Fixed to multi-page React files

---
+++ src/products.js
@@ -0,0 +1,19 @@
+function Products(props){
+    return(
+      <div>
+        <h1>Products</h1>
+        <div>
+          You asked, we delivered: check out our May 2022 new product line!
+          <ul>
+            <li>BR2 logo sticker <br />$4 <br />"Show your dedication to the Great Todd and BR2."</li>
+            <li>BR2 hat <br/> $15 <br/> "Show that your brain is the property of BR2."</li>
+            <li>Marble Bust of Todd <br />$9,999.98<br/>"Gone but immortalized, show your dedication that the Great Todd will one day return to us."</li>
+            <li>Scott Dossey's Private Phone Number<br/>$ [Market Rate]<br/> "I'm not saying we HAVE it, but inquire with our specialists about whether maybe it's a thing* or not.</li>
+            <li>We tell you a story about Todd<br/>$250<br/>"We will sing to you legends of the Great Todd"</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+  export default Products;
