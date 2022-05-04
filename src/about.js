7eda8da619c0e4d96a88e63ef6df7c6636c1d683
davidwarren <dbwarren23@gmail.com>
2022-05-04 12:27:01 -0400

    Added bootstrap navbar and general code cleanup

--- src/about.js
+++ src/about.js
@@ -3,7 +3,7 @@ function About(props){
       <div>
         <h1>About Us</h1>
         <p>
-          We here at Todd Talent South's very own Breakour Room 2 have heard your please, and are proud to offer the following goods and services to our compatriots.
+          We here at Todd Talent South's very own Breakout Room 2 have heard your please, and are proud to offer the following goods and services to our compatriots.
         </p>
         <p>
           Whether you're new to the works of the Great Todd and our ongoing collaborative efforts or an established member, TTS:BR2 is here to help you look and code your best.      
--- src/navbar.js
+++ src/navbar.js
@@ -3,12 +3,33 @@ import './css/index.css';
 
 function Navbar(props){
     return(
-      <div className="nav">
-        <Link to='/'>Home</Link>
-        <Link to='/products'>Products</Link>
-        <Link to='/about'>About</Link>
-      </div>
-    );
-  }
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Todd Talent South Store</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-  export default Navbar;
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <Link to="/" className="nav-link" >Home <span class="sr-only">(current)</span></Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/products" className="nav-link">Products</Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/about" className="nav-link">About</Link>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+      </form>
+    </div>
+  </nav>
+
+
+
+    
+   );
+}
+
+export default Navbar;
