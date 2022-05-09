f167f432756ab1cccfba520ed8eaa274f3051fcf
Justin Baker <100954109+justinBak@users.noreply.github.com>
2022-05-09 17:21:41 -0400

    Modify products.js & product.css

--- src/home.js
+++ src/home.js
@@ -1,25 +1,33 @@
+import './css/index.css';
+
 function Home(props){
     return(
-      <div class="jumbotron">
-        <h1>THE TODD TALENT SOUTH STORE!</h1>
-        <img src="./resources/images/ToddBust.png" 
-        alt="Bust of Todd, circa 500BC, courtesy of the Louvre" 
-        className='logo'/>
-        <img src="./resources/images/toddtslogo.jpg" 
-        alt="Todd Talent South logo" 
-        className='logo'/>
-        <img src="./resources/images/Hat-Merchandise.png" 
-        alt="ToddTS Hat" width="400px" />
-        <h4>The Exclusive Merch Store of Todd Talent South's Breakout Room 2!</h4>
-        <hr class="my-4"/>
-        <p>We have high quality, exclusive Todd merchandise for sale for a limited time!</p>
-        <p>Click "Shop Now!" below too see what we have in stock.</p>
-        <p>Be sure to act quickly as quantities are very limited!</p>
-        <p class="lead">
-        <a class="btn btn-secondary btn-lg" href="products" type="button">SHOP NOW!</a>
-        <span> </span>
-        <a class="btn btn-secondary btn-lg" href="about" type="button">ABOUT US</a>
-        </p>
+      <div class="container container-fluid">
+        <div className='center'>
+          <h1>THE TODD TALENT SOUTH STORE!</h1>
+          <div className='image-container'>
+            <img src="./resources/images/ToddBust.png" 
+            alt="Bust of Todd, circa 500BC, courtesy of the Louvre" 
+            className='logo' width={200}/>
+            <img src="./resources/images/toddtslogo.jpg" 
+            alt="Todd Talent South logo" 
+            className='logo'/>
+            <img src="./resources/images/Hat-Merchandise.png" 
+            alt="ToddTS Hat" width="400px" />
+          </div>
+          <h4 className='h4-info'>The Exclusive Merch Store of Todd Talent South's Breakout Room 2!</h4>
+          <hr class="my-4"/>
+          <p className='info'>
+            We have high quality, exclusive Todd merchandise for sale for a limited time!
+            Click "Shop Now!" below too see what we have in stock.
+            Be sure to act quickly as quantities are very limited!
+          </p>
+          <p class="lead">
+          <a class="btn btn-secondary btn-lg" href="products" type="button">SHOP NOW!</a>
+          <span> </span>
+          <a class="btn btn-secondary btn-lg" href="about" type="button">ABOUT US</a>
+          </p>
+        </div>
       </div>
     );
   }
--- src/products.js
+++ src/products.js
@@ -14,26 +14,26 @@ function Products(props){
           <div className='carousel-position'>
             <div id="carouselExampleCaptions" className="carousel" data-bs-ride="carousel">
               <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active lower-btn1" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" className='lower-btn2' aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" className='lower-btn3' aria-label="Slide 3"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" className='lower-btn2' aria-label="Slide 4"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" className='lower-btn1' aria-label="Slide 5"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active lower-btn1 pointer" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" className='lower-btn2 pointer' aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" className='lower-btn3 pointer' aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" className='lower-btn2 pointer' aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" className='lower-btn1 pointer' aria-label="Slide 5"></button>
               </div>
-              <div class="carousel-inner">
-                <div class="carousel-item active">
+              <div className="carousel-inner">
+                <div className="carousel-item active">
                   <div>
-                    <a className='alert-style' onClick={() => {alert("Show Your dedication to the Great Todd and BR2. For only $4.99 + taxes.")}}>
+                    <a onClick={() => {alert("Show Your dedication to the Great Todd and BR2. For only $4.99 + taxes.")}}>
                       <div className='img-center'>
-                        <img src="./resources/images/toddtslogo.jpg" class="d-block w-25" alt="..."/>
+                        <img src="./resources/images/toddtslogo.jpg" className="d-block w-25 pointer" alt="TODD_TALENT_SOUTH_logo"/>
                       </div>
                     </a>
                   </div>
-                  <div className='item item-position1'>
+                  <div className='item item1-position'>
                     <div className='text-container'>
                       <div className='text-resize'>
                         <h5>BR2 Logo Sticker</h5>
-                        <p className='text-info1'>
+                        <p className='text-info'>
                           Show your dedication to the Great Todd and BR2.
                         </p>
                         <p>&#36;4.99</p>
@@ -41,16 +41,16 @@ function Products(props){
                     </div>
                   </div>
                 </div>
-                <div class="carousel-item">
+                <div className="carousel-item">
                   <div>
                   <a onClick={() => {alert("Show that your brain is the property of BR2. For only $14.99 + taxes.")}}>
                     <div className='center'>
-                      <img src="./resources/images/Hat-Merchandise.png" class="img-position w-50" alt="..."/>
+                      <img src="./resources/images/Hat-Merchandise.png" className="img-position w-50 pointer" alt="white hat with TODD_TALENT_SOUTH_logo image"/>
                     </div>
                   </a>
                   </div>
-                  <div className='item item-position'>
-                    <div className='text-container'>
+                  <div className='item'>
+                    <div>
                       <div className='text-resize'>
                         <h5>BR2 hat</h5>
                         <p className='text-info'>Show that your brain is the property of BR2.</p>
@@ -63,12 +63,12 @@ function Products(props){
                   <div>
                   <a onClick={() => {alert("Gone but immortalized, show your dedication that the Great Todd will one day return to us. For only $999.99 + taxes.")}}>
                     <div className='center'>
-                      <img src="./resources/images/ToddBust.png" class="img-resize img3-position" alt="..."/>
+                      <img src="./resources/images/ToddBust.png" className="img-resize img3-position pointer" alt="Bust of man 'Todd' image"/>
                     </div>
                   </a>
                   </div>
-                  <div className='item item-position'>
-                    <div className='text-container'>
+                  <div className='item'>
+                    <div className='img3-margin'>
                       <div className='text-resize'>
                         <h5>Marble Bust of Todd</h5>
                         <p className='text-info'>Gone but immortalized, show your dedication ... </p> 
@@ -78,16 +78,16 @@ function Products(props){
                     </div>
                   </div>
                 </div>
-                <div class="carousel-item">
+                <div className="carousel-item">
                   <div>
                   <a onClick={() => {alert("I'm not saying we HAVE it, but inquire with our specialists about whether maybe it's a thing* or not. (Call to Inquire)")}}>
                     <div className='center'>
-                      <img src="./resources/images/BigDaddyScott.png" class="w-50 img-position" alt="..."/>
+                      <img src="./resources/images/BigDaddyScott.png" className="w-50 img-position pointer" alt="Scott holding phone in front of his face showing # in a telephone number format"/>
                     </div>            
                   </a>
                   </div>
-                  <div className='item item-position scott-text'>
-                    <div className='text-container'>
+                  <div className='item scott-text'>
+                    <div>
                       <div className='text-resize'>
                         <h5>Scott Dossey's Private #</h5>
                         <p className='text-info'>"I'm not saying we HAVE it, but inquire with ...</p>  
@@ -96,30 +96,30 @@ function Products(props){
                     </div>
                   </div>
                 </div>
-                <div class="carousel-item">
-                  <div className='last-img-position'>
-                  <a onClick={() => {alert("We will sing to you legends of the Great Todd. For only $249.99 + taxes.")}}>
-                    <div className='center'>
-                      <img src="./resources/images/storyTime.png" class="w-50 img-position" alt="..."/>
-                    </div>                
-                  </a>
+                <div className="carousel-item">
+                  <div className='img-margin'>
+                    <a onClick={() => {alert("We will sing to you legends of the Great Todd. For only $249.99 + taxes.")}}>
+                      <div className='center'>
+                        <img src="./resources/images/TaleStory.png" className="w-50 last-img-position pointer" alt="'Tale Story' logo yellow with blue border"/>
+                      </div>                
+                    </a>
                   </div>
-                  <div className='item item-position scott-text'>
-                    <div className='text-container'>
-                      <div className='text-resize'>
+                  <div className='item item-position item-margin'>
+                    <div>
+                      <div className='text-resize last-text-container'>
                         <h5>We tell you a story about Todd</h5>
                         <p className='text-info'>We will sing to you legends of the Great Todd.</p>
-                        <p>&#36;249.99</p>
+                        <p className='text-info'>&#36;249.99</p>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <button class="carousel-control-prev btn-left" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <button className="carousel-control-prev btn-left" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
               </button>
-              <button class="carousel-control-next btn-right" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <button className="carousel-control-next btn-right" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
               </button>
             </div>
           </div>
