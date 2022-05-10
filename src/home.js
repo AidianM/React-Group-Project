74e5acb0033089e35a70d7b368dc9cf33dfddfb7
Justin Baker <100954109+justinBak@users.noreply.github.com>
2022-05-09 20:57:21 -0400

    Modify navbar & home page

--- src/home.js
+++ src/home.js
@@ -2,55 +2,43 @@ import './css/index.css';
 
 function Home(props){
     return(
-<<<<<<< HEAD
-      <div class="container container-fluid">
+      <div className=" container-fluid">
         <div className='center'>
-          <h1>THE TODD TALENT SOUTH STORE!</h1>
-          <div className='image-container'>
-            <img src="./resources/images/ToddBust.png" 
-            alt="Bust of Todd, circa 500BC, courtesy of the Louvre" 
-            className='logo' width={200}/>
-            <img src="./resources/images/toddtslogo.jpg" 
+          <h1 className='container-fluid' id='head-title'>THE TODD TALENT SOUTH STORE!</h1>
+          <div className='image-container container-fluid'>
+            <img 
+              src="./resources/images/ToddBust.png" 
+              alt="Bust of Todd, circa 500BC, courtesy of the Louvre" 
+              width="200px"
+              className='col-md-4'
+            />
+            <img 
+            src="./resources/images/toddtslogo.jpg" 
             alt="Todd Talent South logo" 
-            className='logo'/>
-            <img src="./resources/images/Hat-Merchandise.png" 
-            alt="ToddTS Hat" width="400px" />
+            width="200px"
+            className='col-md-3'
+            />
+            <img 
+              src="./resources/images/Hat-Merchandise.png" 
+              alt="ToddTS Hat" 
+              width="300px"
+              className='col-md-4'
+            />
           </div>
-          <h4 className='h4-info'>The Exclusive Merch Store of Todd Talent South's Breakout Room 2!</h4>
-          <hr class="my-4"/>
-          <p className='info'>
-            We have high quality, exclusive Todd merchandise for sale for a limited time!
-            Click "Shop Now!" below too see what we have in stock.
-            Be sure to act quickly as quantities are very limited!
-          </p>
-          <p class="lead">
-          <a class="btn btn-secondary btn-lg" href="products" type="button">SHOP NOW!</a>
-          <span> </span>
-          <a class="btn btn-secondary btn-lg" href="about" type="button">ABOUT US</a>
-          </p>
+          <div className='container-fluid'>
+            <h4 className='h4-info'>The Exclusive Merch Store of Todd Talent South's Breakout Room 2!</h4>
+            <hr class="my-4"/>
+            <p className='info'>
+              We have high quality, exclusive Todd merchandise for sale for a limited time!
+              Click "Shop Now!" below too see what we have in stock.
+              Be sure to act quickly as quantities are very limited!
+            </p>
+            <div class="lead">
+              <a class="btn-secondary btn-lg btn home-btn" href="products" type="button">SHOP NOW!</a>
+              <a class="btn-secondary btn-lg btn home-btn" href="about" type="button">ABOUT US</a>
+            </div>
+          </div>
         </div>
-=======
-      <div class="jumbotron">
-        <h1>THE TODD TALENT SOUTH STORE!</h1>
-        <img src="./resources/images/ToddBust.png" 
-        alt="Bust of Todd, circa 500BC, courtesy of the Louvre" 
-        className='logo'/>
-        <img src="./resources/images/toddtslogo.jpg" 
-        alt="Todd Talent South logo" 
-        className='logo'/>
-        <img src="./resources/images/Hat-Merchandise.png" 
-        alt="ToddTS Hat" className='logo' />
-        <h4>The Exclusive Merch Store of Todd Talent South's Breakout Room 2!</h4>
-        <hr class="my-4"/>
-        <p>We have high quality, exclusive Todd merchandise for sale for a limited time!</p>
-        <p>Click "Shop Now!" below too see what we have in stock.</p>
-        <p>Be sure to act quickly as quantities are very limited!</p>
-        <p class="lead">
-        <a class="btn btn-secondary btn-lg" href="products" type="button">SHOP NOW!</a>
-        <span> </span>
-        <a class="btn btn-secondary btn-lg" href="about" type="button">ABOUT US</a>
-        </p>
->>>>>>> 5b2f10e2b45f4e1f3223e3caf4beeef7d05d2147
       </div>
     );
   }
--- src/navbar.js
+++ src/navbar.js
@@ -3,32 +3,36 @@ import './css/navbar.css';
 
 function Navbar(props){
     return(
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/"><img src="./resources/images/toddtslogo.jpg" width="30" height="30" 
-        class="d-inline-block align-top" alt=""/> TODD TALENT SOUTH STORE</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <Link to="/" className="nav-link" >HOME <span class="sr-only">(current)</span></Link>
-            </li>
-            <li class="nav-item">
-              <Link to="/products" className="nav-link">PRODUCTS</Link>
-            </li>
-            <li class="nav-item">
-              <Link to="/about" className="nav-link">ABOUT</Link>
-            </li>
-        </ul>
-        <ul class="navbar-nav ms-auto">
-          <a class="btn btn-secondary btn-lg" href="products" type="button">SHOP NOW!</a>
-          <span class="cart"><svg  xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#387780" class="bi bi-cart" viewBox="0 0 16 16">
-          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-          </svg><p>CART</p></span>
-        </ul>
-    </div>
-  </nav>
+      <div className='container-fluid mobile-viewport'>
+        <nav class="navbar navbar-expand-md navbar-light bg-light mobile-viewport">
+          <div className='row col-lg-6'>
+            <a class="navbar-brand" href="/"><img src="./resources/images/toddtslogo.jpg" width="30" height="30" 
+            class="d-inline-block align-top" alt=""/> TODD TALENT SOUTH STORE</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </div>
+          <div class="collapse navbar-collapse col-lg-6" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto col-lg-6 navbar-content">
+              <li class="nav-item">
+                <Link to="/" className="nav-link" >HOME <span class="sr-only">(current)</span></Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/products" className="nav-link">PRODUCTS</Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/about" className="nav-link">ABOUT</Link>
+              </li>
+            </ul>
+            <ul class="navbar-nav ms-auto col-lg-6">
+              <a class="btn btn-secondary btn-lg shop-btn" href="products" type="button"><span>SHOP NOW!</span></a>
+              <span class="cart"><svg  xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#387780" class="bi bi-cart" viewBox="0 0 16 16">
+              <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+              </svg><span className='cart-icon-text'>CART</span></span>
+            </ul>
+          </div>
+        </nav>
+      </div>
    );
 }
 
